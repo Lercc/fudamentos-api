4942882121f79db642360fcb4513a69a723a22ae
lercc <lercc.en@gmail.com>
2021-01-26 14:32:10 -0500

    API INTERNO - LOCAL

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Api\PostController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +15,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// API PARA OTROS SISTEMAS
+// AUTENTIFICACIÓN POR TOKENS
+
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+// Manera de definir rutas del tipo resource solo permitiendo algunos métodos
+// Route::resource('posts', PostController::class)->only(['create', 'edit']);
+
+// Manera de definir rutas del tipo resource exceptuando a algunos métodos
+// Route::resource('posts', PostController::class)->except(['create', 'edit']);
+
+// Manera de definir una ruta del tipo apiResource
+// Route::apiResource('post', PostController::class);
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,13 @@
 <?php
 
+use App\Http\Controllers\Api\PostController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/', function () {
-    return view('welcome');
+// API INTERNO O API LOCAL
+Route::group(['prefix' => 'api'], function () {
+    Route::apiResource('posts', PostController::class);
 });
