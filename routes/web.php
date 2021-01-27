aaf8893db37bf24c8885fbe9e92395146da3a19f
lercc <lercc.en@gmail.com>
2021-01-27 00:28:07 -0500

    laravel ui

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,15 @@
 use App\Http\Controllers\Api\PostController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return redirect('home');
+});
 
 // API INTERNO O API LOCAL
 Route::group(['prefix' => 'api'], function () {
     Route::apiResource('posts', PostController::class);
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
